bf03b2ac55d088234c36746997c2a662ac5d6a30
Harsh Parekh <harsh_parekh@outlook.com>
2021-10-31 21:04:23 -0400

    Initial Schema.

---
+++ processed/schema.sql
@@ -0,0 +1,87 @@
+
+CREATE TABLE Park {
+  ID varchar(25),
+  Name varchar(255),
+  City varchar(25),
+  State varchar(25)
+
+  PRIMARY KEY (ID)
+};
+
+CREATE TABLE Team {
+  ID varchar(25), -- TODO: Drop teamID and clean up the retroID column.
+
+  PRIMARY KEY (ID)
+};
+
+CREATE TABLE TeamName {
+  TeamID varchar(25),
+  Name varchar(255),
+  FirstYear YEAR,
+
+  PRIMARY KEY (TeamID, Name),
+  FOREIGN KEY TeamID REFRENCES Team (ID)
+};
+
+CREATE TABLE Game {
+  ID varchar(25),
+  Park varchar(25) NOT NULL,
+  HomeTeam varchar(25) NOT NULL,
+  AwayTeam varchar(25) NOT NULL,
+  HomeScore varchar(25),
+  AwayScore varchar(25),
+
+  PRIMARY KEY (ID),
+  FOREIGN KEY Park REFRENCES Park (ID),
+  FOREIGN KEY HomeTeam REFRENCES Team (ID),
+  FOREIGN KEY AwayTeam REFRENCES Team (ID)
+};
+
+CREATE TABLE Player {
+  ID varchar(25), -- TODO: Drop PlayerID and migrate PlayerID to RetroID.
+  FirstName varchar(25),
+  LastName varchar(25),
+  BirthCity varchar(25),
+  BirthState varchar(25),
+  BirthCountry varchar(25),
+  BirthDate DATE,
+  Bats ENUM('R', 'L', 'B'),
+  Throws ENUM('R', 'L', 'B'),
+};
+
+CREATE TABLE Event {
+  GameID varchar(25),
+  EventID int UNSIGNED,
+  HomeScore varchar(25),
+  AwayScore varchar(25),
+  Batter varchar(25),
+  Pitcher varchar(25),
+
+  Strikes TINYINT,
+  Ball TINYINT,
+
+  INNING TINYINT,
+  LINEUP TINYINT,
+
+  EventType ENUM(
+    'Unknown event', 'No event', 'Generic out', 'Strikeout', 'Stolen base', 'Defensive indifference', 'Caught stealing',
+    'Pickoff error', 'Pickoff', 'Wild pitch', 'Passed ball', 'Balk', 'Other advance', 'Foul error', 'Walk',
+    'Intentional walk', 'Hit by pitch', 'Interference', 'Error', 'Fielders choice', 'Single', 'Double', 'Triple',
+    'Home run', 'Missing play'
+  ),
+
+  PRIMARY KEY (GameID, EventID),
+  FOREIGN KEY GameID REFRENCES Game (ID),
+  FOREIGN KEY Batter REFRENCES Player (ID),
+  FOREIGN KEY Pitcher REFRENCES Player (ID)
+};
+
+CREATE TABLE EventComment {
+  GameID varchar(25),
+  EventID int UNSIGNED,
+  Comment TEXT,
+
+  PRIMARY KEY (GameID, EventID),
+  FOREIGN KEY GameID REFRENCES Game (ID),
+  FOREIGN KEY GameID REFRENCES Game (ID),
+};
