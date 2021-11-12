1fa39cb2086579912f17083314f2c3b67be5a176
Harsh Parekh <harsh_parekh@outlook.com>
2021-11-12 15:43:26 -0500

    Add docs for fd library.

--- scripts/fd.py
+++ scripts/fd.py
@@ -3,8 +3,12 @@ import pandas as pd
 
 # Helper functions for functional dependencies.
 
-# Print violation of functional dependencies.
 def print_fd(deps, key, dep):
+    """
+    Print violation of functional dependencies.
+
+    :param deps: The functional dependencies as a dictionary of sets.
+    """
     print(key, dep, sep="\t")
     for k, v in deps.items():
         if len(v) > 1:
@@ -12,6 +16,12 @@ def print_fd(deps, key, dep):
 
 # Extract the functional dependencies from the dataframe.
 def fd(pdf, key, dep):
+    """
+    Is the functional dependency key -> dep satisfied?
+
+    If staisfied the print out is empty,
+    otherwise the violation is printed.
+    """
     dets = ddict(set)
     pdf.apply(lambda x: dets[x[key]].add(x[dep]), axis=1)
     print_fd(dets, key, dep)
@@ -19,10 +29,28 @@ def fd(pdf, key, dep):
 
 # Replace based on functional dependencies.
 def replace_fd(pdf, index, key, rmap):
+    """
+    Replace a column corresponding to key with the value in rmap.
+
+    Example:
+    >>> # Replace all occurrences of 1 with "a" and 2 with "b" in the column A.
+    >>> for index in df.index:
+    ...     replace_fd(df, index, "A", {1: "a", 2: "b"})
+    """
     if pdf.loc[index, key] in rmap:
         pdf.loc[index, key] = rmap[pdf.loc[index, key]]
 
 def load_fd_rMap(fd_file):
+    """
+    Load the functional dependencies from the file.
+
+    Example:
+    >>> # Load the functional dependencies playerID -> ID.
+    >>> fd_rMap = load_fd_rMap("playerID.csv")
+    >>> # Replace playerID with ID.
+    >>> for index in df.index:
+    ...     replace_fd(df, index, "playerID", fd_rMap)
+    """
     df = pd.read_csv(fd_file)
     k1, k2 = tuple(df.keys())
     rMap = {
